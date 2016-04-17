54fc608cec67c7d1e3196b284cce98b15b623685
uthaipon <uthaipon@gmail.com>
2016-04-16 21:12:47 -0400

    Added files via upload
    Starting test files. No definition is included in .cpp yet

---
+++ Test.cpp
@@ -0,0 +1,46 @@
+#include "Test.h"
+#include <cassert>
+using namespace std;
+
+//default - create a Test instance with a starting number 0
+Test::Test()
+{
+}
+
+//Test Helper
+void Test::testHelper()
+{
+	//decToHex
+	//etc.
+	cerr << "Pass Test!" << endl;
+}
+
+void Test::testCounter()
+{
+	//do something here
+	cerr << "Pass Test!" << endl;
+}
+
+void Test::testRegisterMemory()
+{
+	//do something here
+	cerr << "Pass Test!" << endl;
+}
+
+void Test::testDataMemory()
+{
+	//do something here
+	cerr << "Pass Test!" << endl;
+}
+
+void Test::testMathUnit()
+{
+	//do something here
+	cerr << "Pass Test!" << endl;
+}
+
+void Test::testMain()
+{
+	//do something here
+	cerr << "Pass Test!" << endl;
+}
