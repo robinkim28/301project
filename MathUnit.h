81f3b569aff851579dc22f17c3e38d6437587df7
uthaipon <uthaipon@gmail.com>
2016-04-17 21:46:56 -0400

    Added files via upload
    Updated a bit. Include the part that the math unit should be created with restricted control (e.g. if create multiplexor or ALU, there should be restriction what operations it can do)

--- MathUnit.h
+++ MathUnit.h
@@ -3,6 +3,7 @@
 
 #include "Helper.h"
 #include <string>
+#include <vector>
 /* This class creates any unit that can compute a mathematical function given two inputs, and will output
 	one number. The operation it does is determined by its control
  */
@@ -12,6 +13,9 @@ class MathUnit
  public:
 	//create the math unit with the control initially set to "0"
   MathUnit();
+  //create the math unit with restricted possible math operations. For example, if it's multiplexor, we want
+  //the operation to be only f(x1,x2)=x1 or f(x1,x2)=x2, and no other operation, so listOperation = {"0","1"}
+  MathUnit(vector<string> listOperation);
 
   //with the current 2 inputs and control, do the operation and put result into the output. This does not change input or control
   //the control determines the operation, so the algorithms is a lot of cases (many if-else checking control value)
@@ -31,9 +35,11 @@ class MathUnit
   string inNumber2;
   //control as a string. Note: this needs to be decided for the form it should be. 
   //It can be string, e.g. "ADD", "SUB", "0", "1", or list of 0,1 like "0010" as in the book
+  //If change the control format, chenge the MathUnit constructor and private datafield as well
   string control;
   //output
   string outNumber;
+  vector<string> listOperation
 };
 
 #endif
