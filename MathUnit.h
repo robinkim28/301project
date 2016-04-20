e52fcbd7d4f2a718a76a845fb421701570680fc3
Robin Kim <robin.g.kim@gmail.com>
2016-04-19 21:16:36 -0400

    comment edited

--- MathUnit.h
+++ MathUnit.h
@@ -4,9 +4,7 @@
 #include "Helper.h"
 #include <string>
 #include <vector>
-/* This class creates any unit that can compute a mathematical function given two inputs, and will output
-	one number. The operation it does is determined by its control. Note that control for this is a string,
-	not 0 or 1
+/* This class creates any unit that can compute a mathematical function given two inputs, and will output one number. The operation it does is determined by its control. Note that control for this is a string, not 0 or 1
  */
 
 class MathUnit
@@ -14,14 +12,13 @@ class MathUnit
  public:
 	//create the math unit with the control initially set to "0"
   MathUnit();
-  //create the math unit with restricted possible math operations. For example, if it's multiplexor, we want
-  //the operation to be only f(x1,x2)=x1 or f(x1,x2)=x2, and no other operation, so listOperation = {"0","1"}
+
+  //create the math unit with restricted possible math operations. For example, if it's multiplexor, we want the operation to be only f(x1,x2)=x1 or f(x1,x2)=x2, and no other operation, so listOperation = {"0","1"}
   //Control are {"0","1","ADD","SUB","EQUAL"}
   //Represent: f(x1,x2)={x1,x2,x1+x2,x1-x2,(x1==x2) [is 1 if x1>x2, and 0 otherwise]} respectively
   MathUnit(vector<string> listOperation);
 
-  //with the current 2 inputs and control, do the operation and put result into the output. This does not change input or control
-  //the control determines the operation, so the algorithms is a lot of cases (many if-else checking control value)
+  //with the current 2 inputs and control, do the operation and put result into the output. This does not change input or control the control determines the operation, so the algorithms is a lot of cases (many if-else checking control value)
   void calculate();
 
   // bunch of setter method
