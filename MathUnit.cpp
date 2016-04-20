33d0dcdf90ec7d8170de84bfc9cc3e2df7c9f47b
Robin Kim <robin.g.kim@gmail.com>
2016-04-19 20:38:06 -0400

    handling overflow

--- MathUnit.cpp
+++ MathUnit.cpp
@@ -29,13 +29,22 @@ void MathUnit::calculate()
 		int num1 = Helper::hexToDec(inNumber1);
 		int num2 = Helper::hexToDec(inNumber2);
 		int sum = num1 + num2;
+		if(Helper::decToHex(sum).length() > 8)
+		{
+			cerr << "overflow" << endl;
+		}
 		outNumber = Helper::decToHex(sum);
+		
 	}
 	else if(control == "SUB")
 	{
 		int num1 = Helper::hexToDec(inNumber1);
 		int num2 = Helper::hexToDec(inNumber2);
-		int sum = num1 - num2;
+		int sum == num1 - num2;
+		if(Helper::decToHex(sum).length() > 8)
+		{
+			cerr << "overflow" << endl;
+		}
 		outNumber = Helper::decToHex(sum);
 	}
 	else if(control == "EQUAL")
@@ -44,7 +53,7 @@ void MathUnit::calculate()
 		int num2 = Helper::hexToDec(inNumber2);
 		if (num1 != num2)
 		{
-			outNumber = "1"; // - equal but why 1 if x1> x2 in .h file?
+			outNumber = "1"; 
 		}
 		else
 		{
