92893f33f23bff008468fc098fd9e101cb8608c9
KFarley1994 <kelly.farley@richmond.edu>
2016-04-21 13:56:43 -0400

    MainControl.cpp complete

--- MainControl.cpp
+++ MainControl.cpp
@@ -24,9 +24,61 @@ MainControl::calculate()
 		outRegDst = true;
 		outJump = false;
 		outBranch = false;
-		outMem
-				outAluOp = inOpcode;
+		outMemRead = false;
+		outMemtoReg = false
+		outAluOp = inOpcode;
+		outMemWrite = 0;
 		outALUSrc = false;
+		outRegWrite = true;
+
+	} else if (inOpcode.getOpcode() == LW) {
+
+		outRegDst = false;
+		outJump = false;
+		outBranch = false;
+		outMemRead = true;
+		outMemtoReg = true;
+		outAluOp = inOpcode;
+		outMemWrite = false;
+		outALUSrc = true;
+		outRegWrite = true;
+		
+	} else if (inOpcode.getOpcode() == SW) {
+
+		outRegDst = false;
+		outJump = false;
+		outBranch = false;
+		outMemRead = false;
+		outMemtoReg = false;
+		outAluOp = inOpcode;
+		outMemWrite = true;
+		outALUSrc = true;
+		outRegWrite = false;
+
+	} else if (inOpcode.getOpcode() == BEQ) {
+
+		outRegDst = false;
+		outJump = false;
+		outBranch = true;
+		outMemRead = false;
+		outMemtoReg = false;
+		outAluOp = inOpcode;
+		outMemWrite = false;
+		outALUSrc = false;
+		outRegWrite = false;
+
+	} else if (inOpcode.getOpcode() == J) {
+
+		outRegDst = false;
+		outJump = true;
+		outBranch = false;
+		outMemRead = false;
+		outMemtoReg = false;
+		outAluOp = inOpcode;
+		outMemWrite = false;
+		outALUSrc = false;
+		outRegWrite = false;
+
 	}
 
 };
